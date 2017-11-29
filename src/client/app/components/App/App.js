29fae6707663f9674b659bed24bd034c167d7abe
Idan Dagan <idandagan1@gmail.com>
2017-11-29 20:48:16 +0200

    Update App.js

--- src/client/app/components/App/App.js
+++ src/client/app/components/App/App.js
@@ -1,12 +1,17 @@
+/** global webgazer:true */
 import React, { Component } from 'react';
 import Dashboard from '../Dashboard/Dashboard';
-import * as ajaxApi from '../../api/ajaxApi/ajaxApi';
+import * as utils from '../../common/utils';
 import './App.css';
 
 export default class App extends Component {
 
     componentDidMount() {
-        ajaxApi.initGroupList();
+        this.initGallery();
+    }
+
+    initGallery = () => {
+        utils.removeGallery();
     }
 
     render() {
