ba05de8fba894c2612ddd3a43d6eb9bd1da74536
Idan Dagan <idandagan1@gmail.com>
2017-11-29 20:14:00 +0200

    Update PictureManager.js

--- src/client/components/PictureManager/PictureManager.js
+++ src/client/components/PictureManager/PictureManager.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import Camera from 'react-camera';
+import Stopwatch from 'timer-stopwatch';
 import * as ajaxApi from '../../api/ajaxApi/ajaxApi';
 import * as utils from '../../common/utils';
 
@@ -11,22 +12,62 @@ export default class PictureManager extends Component {
         super(props);
         this.state = {
             reader: new window.FileReader(),
+            inProccess: false,
+            counter: 3,
+            currentId: '',
+            candidateId: '',
         };
+        setInterval(this.onPictureCapture, 3000);
+        setInterval(this.counterInterval, 1000);
     }
 
     onPictureCapture = () => {
-        const { reader } = this.state;
+        const { reader, inProccess } = this.state;
         const { renderAnalyze } = this.props;
 
+        if (inProccess) {
+            console.log('not capturing');
+            return;
+        }
         this.camera.capture()
             .then((blob) => {
                 reader.readAsDataURL(blob);
                 reader.onloadend = () => {
-                    utils.processImage(reader.result);
+                    utils.processImage(reader.result)
+                        .then(this.afterImageProcess);
                 };
                 const src = URL.createObjectURL(blob);
                 renderAnalyze(src);
             });
+    }
+
+    afterImageProcess = (data) => {
+        console.log('after image proceccing');
+        const { counter, currentId } = this.state;
+        if (!data.Errors) {
+            // Recognized face
+            // const timer = new Stopwatch(6000);
+            // timer.onDone(() => {
+            //     debugger;
+            //     console.log('Timer is complete');
+            // });
+            this.setState({ inProccess: true });
+            if (!currentId) {
+                this.setState({ currentId: data.images[0].transaction.subject_id });
+            } else {
+                this.setState({ candidateId: data.images[0].transaction.subject_id });
+            }
+        }
+    }
+
+    counterInterval = () => {
+        const { counter, currentId, candidateId } = this.state;
+
+        if (counter > 6) {
+            this.setState({ inProccess: false, counter });
+        }
+
+        this.setState({ counter: counter + 1 });
     }
 
     render() {
