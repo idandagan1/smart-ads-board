9d65df9845fe8ae8182b3c730e34b6ea390f62a6
idandagan1 <idandagan1@gmail.com>
2017-11-30 08:58:25 +0200

    Added css

--- src/client/app/components/PictureManager/PictureManager.js
+++ src/client/app/components/PictureManager/PictureManager.js
@@ -7,7 +7,7 @@ import * as utils from '../../common/utils';
 require('tracking');
 require('tracking/build/data/face');
 const delay = 10000;
-const intervals = 5000;
+const intervals = 4000;
 const style = {};
 
 export default class PictureManager extends Component {
@@ -20,14 +20,15 @@ export default class PictureManager extends Component {
             isCapturing: false,
             currentId: '',
             numOfAd: 1,
-            data: '',
+            persons: [],
         };
-        this.startInterval();
         this.startInterval = this.startInterval.bind(this);
         this.onPictureCapture = this.onPictureCapture.bind(this);
         this.onChangeAd = this.onChangeAd.bind(this);
         this.stopInterval = this.stopInterval.bind(this);
         this.startInterval = this.startInterval.bind(this);
+        this.proseccPerson = this.proseccPerson.bind(this);
+        this.startInterval();
     }
 
     componentDidMount() {
@@ -40,10 +41,15 @@ export default class PictureManager extends Component {
             const { isCapturing } = this.state;
             if (e.data.length > 0 && !isCapturing) {
                 this.onPictureCapture();
-            } else {
-                //console.log('not capturing');
             }
         });
+    }
+
+    onChangeAd() {
+        const { numOfAd } = this.state;
+        const { changeAd } = this.props;
+        changeAd((numOfAd % 4) + 1);
+        this.setState({ numOfAd: numOfAd + 1 });
     }
 
     onPictureCapture() {
@@ -58,27 +64,33 @@ export default class PictureManager extends Component {
                 reader.readAsDataURL(blob);
                 reader.onloadend = () => {
                     utils.processImage(reader.result)
-                        .then(data => this.afterImageProcess(data, blob));
+                        .then((res) => {
+                            if (res.Errors) {
+                                utils.getCreatives(reader.result)
+                                    .then((person) => {
+                                        const { persons } = this.state;
+                                        persons[person.personId] = persons;
+                                        this.setState({ persons });
+                                        this.proseccPerson(person);
+                                    });
+                            } else {
+                                const { persons } = this.state;
+                                this.proseccPerson(persons[res.images[0].transaction.subject_id]);
+                            }
+                        });
                 };
             });
     }
 
-    onChangeAd() {
-        const { numOfAd } = this.state;
-        const { changeAd } = this.props;
-        changeAd((numOfAd % 4) + 1);
-        this.setState({ numOfAd: numOfAd + 1 });
-    }
-
-    afterImageProcess(data, blob) {
+    proseccPerson(person) {
         this.setState({ isCapturing: false });
         const { currentId } = this.state;
-        //const { changeAd, renderAnalyze, markedImage } = this.props;
-        if (!data.Errors) {
+        const { changeAd, renderAnalyze, markedImage } = this.props;
+        if (person) {
             // Recognized face
             console.log('found face...');
             this.stopInterval();
-            //renderAnalyze(data);
+            renderAnalyze(person);
             //const img = utils.getCreatives(data);
             //changeAd('https://cdn.static-economist.com/sites/default/files/images/2017/06/articles/main/20170610_ldp501.jpg');
             this.setState({ inProccess: true });
@@ -89,11 +101,11 @@ export default class PictureManager extends Component {
                 this.setState({ inProccess: false, isCapturing: false });
             });
             if (!currentId) {
-                this.setState({ currentId: data.images[0].transaction.subject_id });
-            } else if (currentId === data.images[0].transaction.subject_id) {
+                this.setState({ currentId: person.subject_id });
+            } else if (currentId === person.subject_id) {
                 //this.startInterval();
-            } else if (data.images[0].transaction.status === 'success') {
-                this.setState({ currentId: data.images[0].transaction.subject_id });
+            } else {
+                this.setState({ currentId: person.subject_id });
             }
         } else {
             this.setState({ currentId: '' });
